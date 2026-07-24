bb12375f3c6c150376d14c452831dceca463dbad
Agung Bahtiar <agungbahtiar050@gmail.com>
2026-07-24 07:55:28 +0700

    Add Sitemap integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site:'https://0xab.my.id',
   output: "static",
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
